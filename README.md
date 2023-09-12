3ade7e95c4e54b2aa36dca1959d161135f968f8d
Tim Clevenger <tjclevenger+git@gmail.com>
2023-09-12 16:03:05 -0500

    initial script

---
+++ README.md
@@ -0,0 +1,16 @@
+ta_parser.py:  TrustedAdvisor parser.
+Tim Clevenger (https://github.com/TClevenger) 2023
+
+ta_parser.py is a quick and dirty script that parses AWS TrustedAdvisor
+reports (Excel format) and removes all sheets where the status is "ok"
+or "not_available".  This leaves just the sheets with "warning" or
+"error" statuses, making it much easier to warp through the report to
+find stuff to actually fix.
+
+This script does no error checking (or even basic checking to see if
+it's reading a TrustedAdvisor file), gives no confirmations and
+doesn't back up the original, so use at your own risk.
+
+This script requires the openpyxl library (install with 'pip3 install
+openpyxl')
+
---
+++ ta_parser.py
@@ -0,0 +1,37 @@
+# Quick and dirty AWS TrustedAdvisor parser - removes sheets where the status
+# is "ok" or "not_available" and leaves sheets with warnings and errors.  This
+# does not create backups or prompt for confirmation so use as your own risk.
+# Tim Clevenger 2023 (https://github.com/TClevenger)
+#
+# openpyxl required:  pip3 install openpyxl
+#
+
+from openpyxl import load_workbook
+import sys
+import warnings
+
+# Suppress 'using default style' warning
+warnings.simplefilter("ignore")
+
+if len(sys.argv) == 1:
+  print("usage: python3 ta_parser.py FILE")
+  sys.exit(1)
+
+delete_sheets = []
+wbname = sys.argv[1]
+workbook = load_workbook(wbname)
+
+for x in workbook.sheetnames:
+  sheet = workbook[x]
+  if str(sheet["A4"].value) == "Status: ok" or str(sheet["A4"].value) == "Status: not_available":
+    delete_sheets.append(sheet)
+  else:
+    print("Sheet name: " + str(sheet.title) + " will be kept.")
+
+for x in delete_sheets:
+  print("Deleting sheet " + str(x))
+  workbook.remove_sheet(x)
+
+workbook.save(filename = wbname)
+
+sys.exit(0)
